9a4ad88ab5598dcf3cdead57844d130263c7987d
bala-amypo <bala@amypo.in>
2025-12-17 06:21:18 +0000

    User commit

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -8,14 +8,17 @@ import com.example.demo.service. StudentService;
 
 @RestController
 public class StudentController {
-
-@Autowired
-StudentService ser;
+    @Autowired
+    StudentService ser;
 
 @PostMapping("/adddata")
-public Student createData(@RequestBody Student stu){
-return ser.createData(stu);
+    public Student createData(@RequestBody Student stu){
+        return ser.createData(stu);
+    }
 
-@GetMapping("/fetchrecord")
-public List<Student> fetchRecord(){
-return ser.fetchRecord();
+
+    @GetMapping("/fetchrecord")
+    public List<Student> fetchRecord(){
+        return ser.fetchRecord();
+    }
+}
---
+++ src/main/java/com/example/demo/service/impl/StudentServiceImpl.java
@@ -0,0 +1,25 @@
+import com.example.demo.entity.Student;
+import com.example.demo.repository. StudentRepository;
+import com.example.demo.service.StudentService;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentServiceImpl implements StudentService {
+
+     @Autowired
+     StudentRepository repo;
+
+@Override
+//create (or) Insert data
+public Student createData(Student stu) {
+return repo.save(stu);
+
+}
+
+@Override
+// Fetch all records
+public List<Student> fetchRecord() {
+return repo.findA11();
+}
+}
