fcb3392b2feb97bb36922473b9e1872eb84f9757
bala-amypo <bala@amypo.in>
2025-12-18 07:52:21 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/StudentServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/StudentServiceImpl.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class StuServiceImpl implements StudentServ{
+public class StudentServiceImpl implements StudentServ{
 
     @Autowired
     StudentRepository repo;
